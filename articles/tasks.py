cdc9a555a77871efdba35ba112bbccbefb1aee8a
zuoshifan <sfzuo@bao.ac.cn>
2025-02-14 15:26:47 +0800

    Work for celery task

--- articles/tasks.py
+++ articles/tasks.py
@@ -1,19 +1,37 @@
+import re
 import os
 import logging
+from decouple import config
 from celery import shared_task
 from latextranslate import translate_arxiv
+from django.core.cache import cache
 from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
 
+LOCK_TIMEOUT = config('CENXIV_COMPILE_LOCK_TIMEOUT', default=20 * 60) # seconds
+
 @shared_task
 def download_and_compile_arxiv(arxiv_idv):
     """Task to download an arxiv article src and compile it to pdf."""
-    arxiv_id, version = arxiv_idv.split('v')
+    match = re.match(r"(.*)v(\d+)", arxiv_idv)
+    if not match:
+        logger.error(f"Invalid arxiv_idv format: {arxiv_idv}")
+        return
+
+    arxiv_id, version = match.groups()
     cn_pdf_file = f'{settings.CENXIV_FILE_PATH}/arxiv{arxiv_id}/v{version}/cn_pdf/{arxiv_idv}.pdf'
     if os.path.isfile(cn_pdf_file):
         logger.info(f'Chinese PDF file {cn_pdf_file} exists, do nothing')
-    else:
-        logger.info(f'Begain to download and compile arxiv:{arxiv_idv}')
-        translate_arxiv.main([arxiv_idv, '-o', settings.CENXIV_FILE_PATH])
+        return
+
+    # 分布式锁机制
+    lock_key = f'cenxiv:compile_lock:{arxiv_idv}'
+    # 尝试获取锁（原子操作）
+    if not cache.add(lock_key, 'locked', timeout=LOCK_TIMEOUT):
+        logger.info(f'Task for downloading and compiling arxiv:{arxiv_idv} is already being processed.')
+        return
+
+    logger.info(f'Begain to download and compile arxiv:{arxiv_idv}')
+    translate_arxiv.main([arxiv_idv, '-o', settings.CENXIV_FILE_PATH])
