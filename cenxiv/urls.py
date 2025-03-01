9ce41917de2c45936d59c50369c4cc533c304c4f
zuoshifan <sfzuo@bao.ac.cn>
2025-03-01 15:30:57 +0800

    Add translations for admin

--- cenxiv/urls.py
+++ cenxiv/urls.py
@@ -17,12 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+# from django.views.decorators.csrf import csrf_exempt
+# from django.views.i18n import set_language
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('i18n/setlang/', csrf_exempt(set_language), name='set_language'),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
     path('', include('articles.urls')),
 )
